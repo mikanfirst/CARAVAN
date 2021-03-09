4c21f83beaa0b9b621e65dfc6e3d75431ab362e3
mikanfirst <mmkkmkk4869@gmail.com>
2021-03-09 01:56:33 +0000

    [finish]5-4

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -7,11 +7,12 @@ class BlogsController < ApplicationController
   end
 
   def new
+    @blog = Blog.new
 
   end
 
   def create
-    @blog = Blog.new(blog_params)
+    blog = Blog.new(blog_params)
     blog.save
     redirect_to blogs_path
   end
